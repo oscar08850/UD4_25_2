3d15db57b81255ec83553b472929fd3f9064c369
oscar08850 <oscar08850@gmail.com>
2022-11-16 11:57:02 +0100

    Controllers

---
+++ EJ25-2/src/main/java/com/example/demo/controller/ControladorDepartamentos.java
@@ -0,0 +1,75 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.Departamentos;
+import com.example.demo.service.DepartamentoServiceImpl;
+
+@RestController
+@RequestMapping("/api")
+public class ControladorDepartamentos {
+	
+	@Autowired
+	DepartamentoServiceImpl departamentoServiceImpl;
+	
+	@GetMapping("/departamentos")
+	public List<Departamentos> listarDepartamentos(){
+		return departamentoServiceImpl.listarDepartamentos();
+	}
+	
+	@PostMapping("/departamentos")
+	public Departamentos crearDepartamento(@RequestBody Departamentos departamento) {
+		
+		return departamentoServiceImpl.crearDepartamento(departamento);
+		
+	}
+	
+	
+	@GetMapping("/departamentos/{id}")
+	public Departamentos leerDepartamento(@PathVariable(name="id") Long id) {
+		
+		Departamentos departamento= new Departamentos();
+		
+		departamento=departamentoServiceImpl.leerDepartamento(id);
+		
+		System.out.println("Departamento segun ID: "+departamento);
+		
+		return departamento;
+	}
+	
+	@PutMapping("/departamentos/{id}")
+	public Departamentos actualizarDepartamento(@PathVariable(name="id")Long id,@RequestBody Departamentos departamento) {
+		
+		Departamentos departamento_seleccionado= new Departamentos();
+		Departamentos departamento_actualizado= new Departamentos();
+		
+		departamento_seleccionado= departamentoServiceImpl.leerDepartamento(id);
+
+		departamento_seleccionado.setNombre(departamento.getNombre());
+		departamento_seleccionado.setPresupuesto(departamento.getPresupuesto());
+
+
+		
+		departamento_actualizado = departamentoServiceImpl.actualizarDepartamento(departamento_seleccionado);
+		
+		System.out.println("El departamento actualizado es: "+ departamento_actualizado);
+		
+		return departamento_actualizado;
+	}
+	
+	@DeleteMapping("/departamentos/{id}")
+	public void borrarDepartamento(@PathVariable(name="id")Long id) {
+		departamentoServiceImpl.borrarDepartamento(id);
+	}
+	
+	
+}
---
+++ EJ25-2/src/main/java/com/example/demo/controller/ControladorEmpleados.java
@@ -0,0 +1,72 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.Empleados;
+import com.example.demo.service.EmpleadosServiceImpl;
+
+@RestController
+@RequestMapping("/api")
+public class ControladorEmpleados {
+
+	@Autowired
+	EmpleadosServiceImpl empleadosServiceImpl;
+
+	@GetMapping("/empleados")
+	public List<Empleados> listarEmpleados() {
+		return empleadosServiceImpl.listarEmpleados();
+	}
+
+	@PostMapping("/empleados")
+	public Empleados crearEmpleado(@RequestBody Empleados empleado) {
+
+		return empleadosServiceImpl.crearEmpleado(empleado);
+
+	}
+
+	@GetMapping("/empleados/{dni}")
+	public Empleados leerEmpleado(@PathVariable(name = "dni") String dni) {
+
+		Empleados empleado = new Empleados();
+
+		empleado = empleadosServiceImpl.leerEmpleadoByDni(dni);
+
+		System.out.println("Empleado con ese dni es: " + empleado);
+
+		return empleado;
+	}
+
+	@PutMapping("/empleados/{dni}")
+	public Empleados actualizarEmpleado(@PathVariable(name = "dni") String dni, @RequestBody Empleados empleado) {
+
+		Empleados empleado_seleccionado = new Empleados();
+		Empleados empleado_actualizado = new Empleados();
+
+		empleado_seleccionado = empleadosServiceImpl.leerEmpleadoByDni(dni);
+
+		empleado_seleccionado.setNombre(empleado.getNombre());
+		empleado_seleccionado.setApellidos(empleado.getApellidos());
+		empleado_seleccionado.setDepartamento(empleado.getDepartamento());
+
+		empleado_actualizado = empleadosServiceImpl.actualizarEmpleado(empleado_seleccionado);
+
+		System.out.println("El empleado actualizado es: " + empleado_actualizado);
+
+		return empleado_actualizado;
+	}
+
+	@DeleteMapping("/empleados/{dni}")
+	public void borrarEmpleado(@PathVariable(name = "dni") String dni) {
+		empleadosServiceImpl.borrarEmpleadoByDni(dni);
+	}
+
+}
